f6760e7f2b1f88755ffd3bf090559d158ce83e05
SlimPickens83 <mikerad83@gmail.com>
2023-06-28 13:21:12 -0400

    Added initial registration form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,10 +3,14 @@ import React, { useEffect } from "react"
 function Contact() {
   return (
     <div id="contact">
-      <h3>Call Us</h3>
-      <p>412-555-0000</p>
-      <h3>Email Us</h3>
-      <p>contact@accountabilitypgh.com</p>
+      <div id="contactChild">
+        <h3 className="text-primary">Call Us</h3>
+        <p>412-555-0000</p>
+      </div>
+      <div id="contactChild">
+        <h3 className="text-primary">Email Us</h3>
+        <p>contact@accountabilitypgh.com</p>
+      </div>
     </div>
   )
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,31 @@
 import React, { useEffect } from "react"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
 
 function Register() {
   return (
-    <div id="register">
-      <p>Hi, this is where our registration form will be.</p>
+    <div id="registerContainer">
+      <h1 className="text-primary">Registration</h1>
+      <Form id="register">
+        <Form.Group className="mb-3" controlId="formBasicUsername">
+          <Form.Label>Please create a username.</Form.Label>
+          <Form.Control type="text" placeholder="Enter Username" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Please enter a valid email address.</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Please create a secure password.</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button id="regSubmit" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   )
 }
