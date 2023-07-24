a2048f3a57ec257b5d6b8099603b50b4e33b78ed
SlimPickens83 <mikerad83@gmail.com>
2023-07-24 12:20:46 -0400

    Updated styling for public pages

--- client/src/components/HeaderLoggedIn.jsx
+++ client/src/components/HeaderLoggedIn.jsx
@@ -8,32 +8,34 @@ import logo from "../assets/aa-p_mk2.png"
 
 function HeaderLoggedIn() {
   return (
-    <Navbar id="header" expand="lg" data-bs-theme="light">
-      <div className="alert construction alert-warning">Under construction.</div>
-      <Container id="headerContainer">
-        <div id="headerChild">
-          <Link to="/" className="navbar-brand">
-            <img id="logo" src={logo} />
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" id="headerButton" />
-        </div>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/faq" id="headerGrandchild">
-              FAQ
-            </Nav.Link>
-            <Nav.Link href="/about" id="headerGrandchild">
-              About
-            </Nav.Link>
-            <NavDropdown className="btn-primary" title="Client Portal" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/">Logout</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/commissions">CommissionsPro</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div style={{ marginLeft: "20%", marginRight: "20%" }}>
+      <Navbar id="header" expand="lg" data-bs-theme="light">
+        <div className="alert construction alert-warning">Under construction.</div>
+        <Container id="headerContainer">
+          <div id="headerChild">
+            <Link to="/" className="navbar-brand">
+              <img id="logo" src={logo} />
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" id="headerButton" />
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/faq" id="headerGrandchild">
+                FAQ
+              </Nav.Link>
+              <Nav.Link href="/about" id="headerGrandchild">
+                About
+              </Nav.Link>
+              <NavDropdown className="btn-primary" title="Client Portal" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/">Logout</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/commissions">CommissionsPro</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
