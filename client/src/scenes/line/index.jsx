bc098cfe1c3a508e5d8f0fbb0f0c1d37eafa1956
SlimPickens83 <mikerad83@gmail.com>
2023-07-28 15:46:28 -0400

    Finished build of initial static dashboard

--- client/src/scenes/line/index.jsx
+++ client/src/scenes/line/index.jsx
@@ -1,17 +1,12 @@
 import { Box } from "@mui/material"
 import AdminHeader from "../../components/dashComponents/AdminHeader"
 import LineChart from "../../components/dashComponents/LineChart"
-import { tokens } from "../../theme"
-import { useTheme } from "@mui/material"
 
 function Line() {
-  const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
-
   return (
     <Box m="20px">
       <AdminHeader title="Line Chart" subtitle="Simple Line Chart" />
-      <Box height="75vh" border={`1px solid ${colors.grey[100]}`} borderRadius="4px">
+      <Box height="75vh">
         <LineChart />
       </Box>
     </Box>
